a5cd6c8ac7c7ffea81842ddd55b74033a9edb3cc
SKARMAAN <83514208+SKARMAAN@users.noreply.github.com>
2023-12-31 18:51:43 +0530

    Update vars.py

--- Powers/vars.py
+++ Powers/vars.py
@@ -11,10 +11,10 @@ class Config:
     """Config class for variables."""
 
     LOGGER = True
-    BOT_TOKEN = config("BOT_TOKEN", default=None)
-    API_ID = int(config("API_ID", default="123"))
-    API_HASH = config("API_HASH", default=None)
-    OWNER_ID = int(config("OWNER_ID", default=1344569458))
+    BOT_TOKEN = config("BOT_TOKEN", default="6356729797:AAGdtemVDDyL5C1o6ZXhFkrjwj7FNPy-uH8")
+    API_ID = int(config("API_ID", default="29098103"))
+    API_HASH = config("API_HASH", default="06baef4020832888ccf3ebf4e746d52b")
+    OWNER_ID = int(config("OWNER_ID", default=5332414680))
     MESSAGE_DUMP = int(config("MESSAGE_DUMP"))
     DEV_USERS = [
         int(i)
@@ -37,22 +37,22 @@ class Config:
             default="",
         ).split(" ")
     ]
-    GENIUS_API_TOKEN = config("GENIUS_API",default=None)
-    AuDD_API = config("AuDD_API",default=None)
-    RMBG_API = config("RMBG_API",default=None)
-    DB_URI = config("DB_URI", default="")
-    DB_NAME = config("DB_NAME", default="gojo_satarou")
+    GENIUS_API_TOKEN = config("GENIUS_API",default="_SKHd_-xt05rO7pSt2Zvx4pL1_MoBOYWjOOPd-pQ1zinaHzmOYBajdVa_rrBKJgl")
+    AuDD_API = config("AuDD_API",default="02342fd5abeb2840f7ff0522a6d8c55f")
+    RMBG_API = config("RMBG_API",default="qjzqR82wn2BCkxUazpQ7vP5H")
+    DB_URI = config("DB_URI", default="mongodb+srv://godgamer9434:vf3MXDyCAWT7I2S7@cluster0.yx21dk8.mongodb.net/?retryWrites=true&w=majority")
+    DB_NAME = config("DB_NAME", default="godgamer9434")
     BDB_URI = config("BDB_URI",default=None)
     NO_LOAD = config("NO_LOAD", default="").split()
     PREFIX_HANDLER = config("PREFIX_HANDLER", default="/").split()
-    SUPPORT_GROUP = config("SUPPORT_GROUP", default="gojo_bots_network")
-    SUPPORT_CHANNEL = config("SUPPORT_CHANNEL", default="gojo_bots_network")
+    SUPPORT_GROUP = config("SUPPORT_GROUP", default="Grab_Your_WH_Group")
+    SUPPORT_CHANNEL = config("SUPPORT_CHANNEL", default="WofBotsUpdates")
     WORKERS = int(config("WORKERS", default=16))
     TIME_ZONE = config("TIME_ZONE",default='Asia/Kolkata')
-    BOT_USERNAME = ""
-    BOT_ID = ""
-    BOT_NAME = ""
-    owner_username = ""
+    BOT_USERNAME = "Gojo_Satoru_The_Honoured_One_Bot"
+    BOT_ID = "6356729797"
+    BOT_NAME = "Gojo Satoru"
+    owner_username = "Armaan512"
 
 
 class Development:
@@ -60,23 +60,23 @@ class Development:
 
     # Fill in these vars if you want to use Traditional method of deploying
     LOGGER = True
-    BOT_TOKEN = "YOUR BOT_TOKEN"
-    API_ID = 12345  # Your APP_ID from Telegram
-    API_HASH = "YOUR API HASH"  # Your APP_HASH from Telegram
-    OWNER_ID = 1344569458  # Your telegram user id defult to mine
-    MESSAGE_DUMP = -100845454887  # Your Private Group ID for logs
-    DEV_USERS = []
+    BOT_TOKEN = "6356729797:AAGdtemVDDyL5C1o6ZXhFkrjwj7FNPy-uH8"
+    API_ID = 29098103  # Your APP_ID from Telegram
+    API_HASH = "06baef4020832888ccf3ebf4e746d52b"  # Your APP_HASH from Telegram
+    OWNER_ID = 5332414680  # Your telegram user id defult to mine
+    MESSAGE_DUMP = -1001932566732  # Your Private Group ID for logs
+    DEV_USERS = [5332414680]
     SUDO_USERS = []
     WHITELIST_USERS = []
-    DB_URI = ""  # Your mongo DB URI
-    DB_NAME = ""  # Your DB name
+    DB_URI = "mongodb+srv://godgamer9434:vf3MXDyCAWT7I2S7@cluster0.yx21dk8.mongodb.net/?retryWrites=true&w=majority"  # Your mongo DB URI
+    DB_NAME = "godgamer9434"  # Your DB name
     NO_LOAD = []
-    GENIUS_API_TOKEN = ""
-    RMBG_API = ""
+    GENIUS_API_TOKEN = "_SKHd_-xt05rO7pSt2Zvx4pL1_MoBOYWjOOPd-pQ1zinaHzmOYBajdVa_rrBKJgl"
+    RMBG_API = "qjzqR82wn2BCkxUazpQ7vP5H"
     PREFIX_HANDLER = ["!", "/","$"]
-    SUPPORT_GROUP = "SUPPORT_GROUP"
-    SUPPORT_CHANNEL = "SUPPORT_CHANNEL"
+    SUPPORT_GROUP = "Grab_Your_WH_Group"
+    SUPPORT_CHANNEL = "WofBotsUpdates"
     VERSION = "VERSION"
     TIME_ZONE = 'Asia/Kolkata'
-    BDB_URI = ""
+    BDB_URI = "mongodb+srv://SKARMAAN:jAXllZggCqEN3eLe@cluster0.vlcmrpo.mongodb.net/?retryWrites=true&w=majority"
     WORKERS = 8
